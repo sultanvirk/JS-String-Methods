bdbf485d470e9beab1eb305142ffa4f8ed692312
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-31 16:20:46 +0500

    charAt strinf method

---
+++ charAt-string-method.js
@@ -0,0 +1,8 @@
+const str = 'hello world'
+
+console.log(str[1]) // checking the value on index
+// similarly charAt() method works
+console.log(str.charAt(6))
+
+//finding last element
+console.log(str[str.length-1])
