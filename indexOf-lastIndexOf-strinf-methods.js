88f53d28223e752e4b1a0658babfa183b4d36d75
Sultan-Iftikhar <sultaniftikhar918@gmail.com>
2024-05-31 16:36:40 +0500

    updates

---
+++ indexOf-lastIndexOf-strinf-methods.js
@@ -0,0 +1,4 @@
+const str = 'I do code and i love to code'
+
+console.log(str.indexOf('code')) // indexOf method returns the first finded element, return -1 if element not found
+console.log(str.lastIndexOf('code')) //lastIndexOf retuns the last targeted element in array, if the targeted element is not twise in array it will return the values same as indexOf method
---
+++ slice-and-substring-string-methods.js
@@ -0,0 +1,4 @@
+const str = "The morning is upon us"
+
+console.log(str.slice(-8, -4))
+console.log(str.substring(-1)) // it will return whole string in the case of -ve value
